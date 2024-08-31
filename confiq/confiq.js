bb39fad188e00d25ea5a3a80ebf91419c6bf2559
unknown <atikurahad@gmail.com>
2024-08-31 23:33:55 +0600

    first commit

---
+++ confiq/confiq.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+
+const dev ={
+
+    app:{
+        port:process.env.port || 3001
+    },
+    db:{
+        url:ghh,
+    }
+}
---
+++ controllers/userController.js
@@ -0,0 +1,12 @@
+const {v4: uuidv4} = require('uuid')
+
+
+const getAllUsers = async(req,res)=>{
+ try {
+    const users = user.find()
+    
+ } catch (error) {
+    
+ }
+
+}
