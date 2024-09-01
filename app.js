fd3b45b20812259b3ed39cbfa66a9fea8813adc2
unknown <atikurahad@gmail.com>
2024-09-01 20:47:03 +0600

    router use and setup

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 const express = require('express')
+const cors = require('cors')
 const app = express();
+const userRouter = require("./routes/user.route")
 
-
-app.use(express.urlencoded({}))
+app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+app.use(cors())
+app.use("/api/users", userRouter)
+
+
+app.get("/",(req,res)=>{
+    res.sendFile( __dirname+"/./views/index.html")
+})
+
+
+
+//invalid api request 
+app.use((req,res,next )=>{
+    res.status(404).json({
+        message:"route not found"
+    })
+})
+
+//server site error
+app.use((err,req,res,next )=>{
+    res.status(500).json({
+        message:"there ia a server site error"
+    })
+})
+
+
 
 module.exports=app;
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require("./app")
 
 
-app.get("/",(req,res)=>{
-    res.send("Hello")
+app.listen(3001,(req,res)=>{
+    console.log("Server is running on port 3001");
 })
